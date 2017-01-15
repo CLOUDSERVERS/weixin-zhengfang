290992e64f09df8b187db6ad84ed11a36ba1c348
liaol <yicoliao@gmail.com>
2017-01-15 18:24:46 +0800

    update config

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,6 @@
 <?php
 	$config = array(
-		'jxglurl' => 'http://210.34.213.87/',	
+		'jxglurl' => 'http://210.34.214.87/',	
 		'mysql_host'=>getenv('MYSQL_PORT_3306_TCP_ADDR'),
 		'mysql_port'=>getenv('MYSQL_PORT_3306_TCP_PORT'),
 		'mysql_user'=>getenv('MYSQL_USERNAME'),
